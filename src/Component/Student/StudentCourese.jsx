22eafdc6c7776688c1f4ff8bc0a76a773e612480
myFirstgitrepo <tanvirmahin607@gmail.com>
2025-02-19 18:34:13 +0600

    added a total course in student dash

---
+++ src/Component/Student/StudentCourese.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { FaBook } from "react-icons/fa";
+
+export default function StudentCourse({bookedSessions}) {
+  return (
+    <div className="card border-black border boxFixed w-64">
+      <div className="card-body flex-row gap-6 items-center">
+        <FaBook className="text-5xl" />
+        <div>
+          <h2 className="text-xl font-extrabold"> Courses</h2>
+          <h1 className="text-3xl font-bold">{bookedSessions}</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Page/Dashboard/Student/StudentDash.jsx
+++ src/Page/Dashboard/Student/StudentDash.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import StudentBanner from '../../../Component/Student/StudentBanner'
+import useBooked from '../../../Hooks/useBooked'
+import StudentCourse from '../../../Component/Student/StudentCourese'
 
 export default function StudentDash() {
+    const {bookedSessions} = useBooked()
   return (
     <div>
       <StudentBanner />
+
+      <div>
+        <StudentCourse bookedSessions={bookedSessions.length} />
+      </div>
     </div>
   )
 }
